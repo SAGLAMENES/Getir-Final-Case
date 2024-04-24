cfb21678df4ea6b3157baefefa4cbb232ee911dc
Enes Saglam <108627652+SAGLAMENES@users.noreply.github.com>
2024-04-24 22:59:12 +0300

    son ayarlar yapılıyor

---
+++ GetirFinalApp/ShoppingCart/ShoppingCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShoppingCell.swift
+//  GetirFinalApp
+//
+//  Created by Enes Saglam on 24.04.2024.
+//
+
+import UIKit
+
+class ShoppingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
